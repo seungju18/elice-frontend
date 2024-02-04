99bc56103be5e8214966d504fdbe5c64b37a4c35
seungju18 <43843214+seungju18@users.noreply.github.com>
2024-02-04 20:08:51 +0900

    기초 환경 세팅

---
+++ elice-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <a>Elice-frontend</a>
+    </div>
+  );
+}
+
+export default App;
