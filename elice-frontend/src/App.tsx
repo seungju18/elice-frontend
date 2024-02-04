2da27bf5c023c53a2247f29152ee6bf406ebf958
seungju18 <43843214+seungju18@users.noreply.github.com>
2024-02-04 20:58:30 +0900

    #hotfix Fix App and index

--- elice-frontend/src/App.tsx
+++ elice-frontend/src/App.tsx
@@ -1,13 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import { styled } from "styled-components";
 import "./App.css";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div>
-      <a>Elice-frontend</a>
-    </div>
+    <AppDiv className="App">
+      <MainDiv>
+        <MainPage />
+      </MainDiv>
+    </AppDiv>
   );
 }
 
 export default App;
+
+const AppDiv = styled.div`
+  background-color: #f3f4f6;
+  min-height: 100vh;
+`;
+
+const MainDiv = styled.div`
+  margin: auto;
+  width: 100%;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
