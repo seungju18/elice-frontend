534f2107956e6c541c0ea62b99c146178d70187b
seungju18 <43843214+seungju18@users.noreply.github.com>
2024-02-05 00:41:24 +0900

    #feat Create Pagination

---
+++ elice-frontend/src/styles/Pagination.style.ts
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components'
+
+export const PageDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const IconDiv = styled.div``
+
+export const PageNum = styled.span`
+  margin: 0 10px;
+  .highlight {
+    font-size: bold;
+  }
+`
