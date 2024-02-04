8413b72155605f16dedc59980379735037e1b0a0
seungju18 <43843214+seungju18@users.noreply.github.com>
2024-02-04 22:46:52 +0900

    #bugfix CORS로 인해 fetch대신 axios 사용

--- elice-frontend/src/api/getCourses.tsx
+++ elice-frontend/src/api/getCourses.tsx
@@ -1,38 +1,38 @@
-import { FilterCondition } from "../types/filterType";
+import axios from 'axios'
+import { FilterCondition } from '../types/filterType'
 
-const OFFSET = 0;
-const COUNT = 20;
+const OFFSET = 0
+const COUNT = 20
 
 export async function GetCourse(
   conditions: FilterCondition,
   offset: number = OFFSET,
-  count: number = COUNT
+  count: number = COUNT,
 ) {
   try {
-    const res = await fetch(
-      "https://api-rest.elice.io/org/academy/course/list?filter_conditions=" +
-        encodeURIComponent(
-          JSON.stringify({
+    const res = await axios.get(
+      'https://api-rest.elice.io/org/academy/course/list/',
+      {
+        params: {
+          filter_conditions: JSON.stringify({
             $and: [
-              { title: `%${conditions.title || ""}%` },
+              { title: `%${conditions.title || ''}%` },
               {
                 $or: conditions.isFree.map((value) => ({
                   enroll_type: 0,
-                  is_free: value === "free",
+                  is_free: value === 'free',
                 })),
               },
             ],
-          })
-        ) +
-        "&offset=" +
-        offset +
-        "&count=" +
-        count
-    );
-    if (!res.ok) throw new Error("HTTP error! status : ${res.status}");
-    const data = await res.json();
-    return data;
+          }),
+          offset: offset,
+          count: count,
+        },
+      },
+    )
+    console.log(res.data)
+    return res.data
   } catch (e) {
-    alert(e);
+    alert(e)
   }
 }
