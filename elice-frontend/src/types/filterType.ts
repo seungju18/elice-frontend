29f2ff844a757694da46ed7e16d86a84679a3335
seungju18 <43843214+seungju18@users.noreply.github.com>
2024-02-05 15:08:54 +0900

    #hotfix Add CourseTypes on filterType

--- elice-frontend/src/types/filterType.ts
+++ elice-frontend/src/types/filterType.ts
@@ -5,4 +5,7 @@ export interface CourseType {
   image_file_url: string | null
   logo_file_url: string | undefined
   price: string
+  is_discounted: boolean
+  discount_rate: string | null
+  discounted_price: string | null
 }
